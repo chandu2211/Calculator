f05ad988f962c6c7a0a44d218eed1ae0d6fb26c7
chandra <chandra.mamillapalli4a2@gmail.com>
2025-06-02 03:10:20 -0700

    adding

--- add.py
+++ add.py
@@ -3,7 +3,5 @@ def add(x, y,z,a):
   """This function adds two numbers."""
   return x + y +z+a
 =======
-def add(x, y):
-  """This function adds two numbers."""
-  return x + y
+
 >>>>>>> parent of 5a2a55e (adding 3 numbers)
